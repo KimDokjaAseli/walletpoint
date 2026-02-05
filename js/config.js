e92d6bde67614fa2f9d1b84cfeb09d9048b68f24
Kim Dokja <memskie324@gmail.com>
2026-02-06 06:16:46 +0700

    as

--- js/config.js
+++ js/config.js
@@ -1,8 +1,9 @@
 const CONFIG = {
     // Configuration for Production Hosting
-    BASE_URL: 'https://bewallet-point-production.up.railway.app',
-_BASE_URL: 'https://bewallet-point-production.up.railway.app/api/v1',
+    BASE_URL: 'bewallet-point-production.up.railway.app',
+_BASE_URL: 'bewallet-point-production.up.railway.app/api',
     // BASE_URL: 'http://localhost:8102',
     // API_BASE_URL: 'http://localhost:8102/api/v1',
     DEBUG: false
 };
+
