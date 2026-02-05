e1f70d20df3e4df286fe98f95c8f13b3d268f31d
Kim Dokja <memskie324@gmail.com>
2026-02-06 06:01:24 +0700

    asa

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     // Configuration for Production Hosting
-    BASE_URL: 'https://walletpoint.xeroon.my.id',
-    API_BASE_URL: 'https://walletpoint.xeroon.my.id/api/v1',
+    BASE_URL: 'https://bewallet-point-production.up.railway.app',
+_BASE_URL: 'https://bewallet-point-production.up.railway.app/api/v1',
     // BASE_URL: 'http://localhost:8102',
     // API_BASE_URL: 'http://localhost:8102/api/v1',
     DEBUG: false
